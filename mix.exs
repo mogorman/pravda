5f29ab19674469612f854bbff04b757667d018a1
Matthew O'Gorman <mog@rldn.net>
2019-12-14 00:16:50 -0500

    fixing dialyzer i hope

--- mix.exs
+++ mix.exs
@@ -9,6 +9,16 @@ defmodule Pravda.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
+      dialyzer: [
+        plt_add_apps: [:ex_unit, :mix],
+      ],
+
     ]
   end
 
