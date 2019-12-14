74096ce0eae6ee2fac7c30b657e5ec83e74edd51
Matthew O'Gorman <mog@rldn.net>
2019-12-14 00:12:39 -0500

    build dependency things

--- mix.exs
+++ mix.exs
@@ -7,22 +7,25 @@ defmodule Pravda.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:freedom_formatter, "~> 1.0", only: :dev},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.1.0", runtime: false}, 
     ]
   end
 end
