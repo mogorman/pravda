25681cea80f8d8a93fe9d1a36d1bd4a0e77cfd3e
Matthew O'Gorman <mog@rldn.net>
2019-12-16 04:00:59 -0500

    add specs and moduledoc

--- lib/pravda.ex
+++ lib/pravda.ex
@@ -6,7 +6,8 @@ defmodule Pravda do
   @doc ~S"""
   Returns the version of the currently loaded Pravda, in string format.
   """
-  def version do
+  @spec version() :: String.t()
+  def version() do
     Application.loaded_applications()
     |> Enum.map(fn {app, _, ver} -> if app == :pravda, do: ver, else: nil end)
     |> Enum.reject(&is_nil/1)
--- lib/pravda/remote_resolver.ex
+++ lib/pravda/remote_resolver.ex
@@ -1,4 +1,12 @@
 defmodule Pravda.RemoteResolver do
+  @moduledoc """
+  Remote Resolver is used by ex_json_schema to presolve remote refs over http
+  """
+
+  @doc ~S"""
+  resolve json schema.
+  """
+  @spec resolve(String.t()) :: Map.t()
   def resolve(url) do
     {:ok, result} = Mojito.get(url)
     result.body |> Jason.decode!()
