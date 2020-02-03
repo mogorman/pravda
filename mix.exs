5e983cb6863e0e73a8a7096f8342745e7d2b2daa
Matthew O'Gorman <mog@rldn.net>
2020-02-03 16:03:47 -0500

    0.3.1: error call back support and cleanup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pravda.MixProject do
   def project do
     [
       app: :pravda,
-      version: "0.2.2",
+      version: "0.3.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
